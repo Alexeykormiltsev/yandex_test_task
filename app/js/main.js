b7d046d6878f35291d2a7d67f68348f1d9e9848a
Alexeykormiltsev <cormiltsev@yandex.ru>
2023-09-21 17:04:07 +0500

    new3

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,22 @@
 $(function(){
 
+
+    const swiper = new Swiper('.slider-team', {
+
+        direction: 'horizontal',
+        loop: false,
+        autoplay: {
+            delay: 3000,
+        },
+        speed: 1000,
+        spaceBetween: 36,
+        pagination: false,
+        navigation: true,
+    
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+          },
+    });
+    
 });
